30dc55b41b0d15f02cbd8c39d0d7b582690338d1
Ankit7Das <ankit_1901ee74@iitp.ac.in>
2021-08-18 00:49:31 +0530

    delete barber api added

---
+++ Backend/Admins/src/delbarb.js
@@ -0,0 +1,156 @@
+require('dotenv').config();
+
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+var sns = new AWS.SNS({apiVersion: '2010-03-31'});
+var documentClient = new AWS.DynamoDB.DocumentClient({ region: 'ap-south-1' });
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+var nodemailer = require('nodemailer');
+const { userVerifier } = require("./authentication");
+const { passwordGenerator, hashPassword } = require('./password');
+
+exports.handler = async (event) => {
+    try {
+
+        var obj = JSON.parse(event.body);
+        var barberId = obj.barberId;
+        var tokenArray = event.headers.Authorization.split(" ");
+        var token = tokenArray[1];
+
+        if(token == null) {
+            return {
+                statusCode: 401,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    success: false,
+                    message: "No token passed"
+                })
+            };
+        }
+
+        var userID;
+
+        try {
+            userID = jwt.verify(token, JWT_SECRET);
+        } catch(err) {
+            return {
+                statusCode: 403,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    success: false,
+                    message: "Invalid Token",
+                })
+            };
+        }
+
+        var exist1 = await userVerifier(userID.id);
+
+        if(exist1.success == false) {
+            return {
+                statusCode: 404,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    success: false,
+                    message: 'User not found',
+                })
+            }
+        }
+
+        if(exist1.user.role != 'admin') {
+            return {
+                statusCode: 400,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    success: false,
+                    message: 'Not an admin',
+                })
+            }
+        }
+
+        var params = {
+            TableName: 'Users',
+            Key:{
+                id: barberId
+            }
+        }
+
+        var data = await documentClient.get(params).promise();
+
+        if(data.Item) {
+            params = {
+                TableName: 'Users',
+                Key:{
+                    id: barberId
+                }
+            }
+        
+            try {
+                data = await documentClient.delete(params).promise();
+
+                console.log(data);
+
+                return {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify({
+                        success: true,
+                        message: 'Barber deleted',
+                    })
+                }
+            } catch(err) {
+                return {
+                    statusCode: 500,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify({
+                        success: false,
+                        message: err,
+                    })
+                }
+            }
+        } else {
+            console.log("outside");
+            return {
+                statusCode: 400,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    success: false,
+                    message: 'No such barber exists',
+                })
+            }
+        }
+        
+    } catch(err) {
+        console.log(err);
+        return err;
+    }
+}
